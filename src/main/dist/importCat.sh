30b5c7118ae706342fc19b64d5c01466e0c05050
llamersmcw <llamers@mcw.edu>
2022-12-13 11:18:21 -0600

    Update importCat.sh

--- src/main/dist/importCat.sh
+++ src/main/dist/importCat.sh
@@ -5,7 +5,10 @@
 . /etc/profile
 APPNAME="gwas-catalog-pipeline"
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
-
+EMAILLIST=mtutaj@mcw.edu,llamers@mcw.edu
+if [ "$SERVER" == "REED" ]; then
+  EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu,jrsmith@mcw.edu,akwitek@mcw.edu
+fi
 APPDIR=/home/rgddata/pipelines/$APPNAME
 cd $APPDIR
 
@@ -13,4 +16,4 @@ java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/$APPNAME.jar --importAssoc"$@" > run.log 2>&1
 
-[ -s $APPDIR/logs/summary.log ] && mailx -s "[$SERVER] GWAS Catalog Pipeline Run" mtutaj@mcw.edu,llamers@mcw.edu < $APPDIR/logs/summary.log
+[ -s $APPDIR/logs/summary.log ] && mailx -s "[$SERVER] GWAS Catalog Pipeline Run" $EMAILLIST < $APPDIR/logs/summary.log
