dbc05b4c5a710768db26dd9378c4703b25538750
llamersmcw <llamers@mcw.edu>
2025-03-18 11:57:18 -0500

    Update assignVariantForRatGwas.sh

--- src/main/dist/assignVariantForRatGwas.sh
+++ src/main/dist/assignVariantForRatGwas.sh
@@ -5,9 +5,9 @@
 . /etc/profile
 APPNAME="gwas-catalog-pipeline"
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
-EMAILLIST=mtutaj@mcw.edu,llamers@mcw.edu
+EMAILLIST="mtutaj@mcw.edu llamers@mcw.edu"
 if [ "$SERVER" == "REED" ]; then
-  EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu,jrsmith@mcw.edu,akwitek@mcw.edu
+  EMAILLIST="llamers@mcw.edu mtutaj@mcw.edu jrsmith@mcw.edu akwitek@mcw.edu"
 fi
 APPDIR=/home/rgddata/pipelines/$APPNAME
 cd $APPDIR
@@ -16,4 +16,4 @@ java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/$APPNAME.jar --addVariantIds"$@" > run.log 2>&1
 
-[ -s $APPDIR/logs/summary.log ] && mailx -s "[$SERVER] GWAS Catalog Pipeline Run" $EMAILLIST < $APPDIR/logs/ratSummary.log
+[ -s $APPDIR/logs/ratSummary.log ] && mailx -s "[$SERVER] Rat GWAS Pipeline Run" $EMAILLIST < $APPDIR/logs/ratSummary.log
